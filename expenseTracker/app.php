8174b7c971230706ee5cfa6cf399460b1381e517
nishabano <109799621+nishabano@users.noreply.github.com>
2023-07-29 18:46:31 +0530

    Update app.php

--- expenseTracker/app.php
+++ expenseTracker/app.php
@@ -1,1 +1,67 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Expense Tracker</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/app.css'>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Girassol&display=swap"
+      rel="stylesheet"
+    />
+</head>
+<body>
+    <main>
+        <header>
+            <h2><span>Expense Tracker</span><a href="../index.php">Logout</a></h2>
+        </header>
+        <div class="container">
+            <div class="budget__numbers">
+                <div id="earned" class="number">
+                    <small>Total Income</small>
+                    <div class="amount__container">
+                        <p id="earned__symbol">&#8377;</p>
+                    <span id="amount__earned"></span>
+                </div>
+            </div>
+            <div id="available" class="number">
+                <small>Current Balance</small>
+                <div class="amount__container">
+                    <p>&#8377;</p>
+                    <span id="amount__available"></span>
+                </div>
+            </div>
+            <div id="spent" class="number">
+                <small>Total Expense</small>
+                <div class="amount__container">
+                    <p id="spent__symbol">&#8377;</p>
+                    <span id="amount__spent"></span>
+                </div>
+            </div>
+        </div>
+        <div class="input__area">
+            <p>Add transaction</p><input type="text" id="description" placeholder="Description" autofocus>
+            <text id="rupee">&#8377;</text>
+            <input type="number" id="amount" placeholder="Amount">
+        </div>
+        <div class="buttons__area">
+            <button id="add__income">Add Income</button>
+            <button id="add__expense">Add Expense</button>
+        </div>
+        <div class="items__container">
+            <div id="income__container" class="container">
+                <h2>Income</h2>
+            </div>
+            <div id="expenses__container" class="container">
+                <h2>Expenses</h2>
+            </div>
+        </div>
+    </div>
+    <footer>
+    <p>Created by <a href="https://www.linkedin.com/in/nisha-bano-809898245/">Nisha Bano</a></p>
+    </footer>
+</main>
+<script src='../js/script.js'></script>
+</body>
+</html>
